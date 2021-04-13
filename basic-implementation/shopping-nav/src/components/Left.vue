a8ff04c2917e632151982e5bf279e6b5cf76614c
jamieshui <jamie_shui@yeah.net>
2021-04-13 17:57:04 +0800

    shopping-nav demo completed

---
+++ basic-implementation/shopping-nav/src/components/Left.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="title">热门推荐</div>
+        <ul class="menu">
+            <li @click="menu1">笔记本电脑</li>
+            <li @click="menu2">手机</li>
+            <li>服饰</li>
+            <li>手表</li>
+            <li>书籍</li>
+            <li>玩具</li>
+            <li>小家电</li>
+            <li>学习用品</li>
+            <li>办公用品</li>
+        </ul>
+    </div>
+</template>
+<style type="text/css">
+    .title{
+        width: 100px;
+        height: 30px;
+        color: red;
+    }
+    .menu li{
+        list-style: none;
+        height: 50px;
+        margin-bottom: 2.5px;
+        background-color: white;
+        line-height: 50px;
+        cursor: pointer;
+    }
+</style>
+<script type="text/javascript">
+    import Msg from './msg.js'
+    export default{
+        methods:{
+            menu1:function(){
+                Msg.$emit("val", "1");
+            },
+            menu2:function(){
+                Msg.$emit("val", "2");
+            }
+        }
+    }
+</script>
---
+++ basic-implementation/shopping-nav/src/components/goodList.vue
@@ -0,0 +1,65 @@
+<template>
+    <div name="show">
+        <ul class="goodList">
+            <li v-for="good in list">
+                <img v-bind:src="good.img">
+                <p>{{good.goodsName}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style type="text/css">
+    .goodList li{
+        width: 200px;
+        height:100px;
+        list-style: none;
+        float: left;
+        font-size: 9px;
+        color: red;
+        margin-bottom: 50px;
+    }
+    .goodList img{
+        width: 120px;
+        height: 120px;
+    }
+</style>
+
+<script type="text/javascript">
+export default {
+    name:"show",
+    data(){
+        var obj=this;
+        var url="";
+        if(obj.goodId==0) {
+            url="json/bjb.json";
+        }
+        this.$http.get(url).then(function(res){
+            obj.list=res.data;
+        })
+        return {
+            list:[]
+        }
+    },
+    props:{
+        goodId:Number
+    },
+    watch:{
+        goodId(){
+            var obj = this;
+            var url = "";
+            if(obj.goodId==1) {
+                url="json/bjb.json";
+            } else if (obj.goodId==2){
+                url="json/sj.json";
+            }
+            this.$http.get(url).then(function(res){
+            obj.list=res.data;
+            })
+            return {
+                list:[]
+            }
+        }
+    }
+}
+</script>
